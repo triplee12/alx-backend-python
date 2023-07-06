a0cc0c72c9f7b02158dcb5ef4510de5f96924a2e
triplee12 <tripleeoliver@gmail.com>
2023-07-06 19:23:19 +0100

    Basic annotation - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Basic annotation - add."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Addition of two float.
+
+    Args:
+        a (float): The first float to add
+        b (float): The second float to add
+
+    Returns:
+        Return the result of the added float
+    """
+    return a + b
