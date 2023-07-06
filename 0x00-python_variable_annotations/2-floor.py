bbc7e5c95bf9e6c1923088dc0d8ce91412e35203
triplee12 <tripleeoliver@gmail.com>
2023-07-06 19:43:16 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Basic annotations - floor."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Floor a float number.
+
+    Args:
+        n (float): float number to floor
+
+    Returns:
+        int: the floor number
+    """
+    return math.floor(n)
